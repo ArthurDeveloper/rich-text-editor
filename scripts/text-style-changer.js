ced0bbf515956b7654a46296a44f29b33c05a69b
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2021-07-19 19:01:55 -0300

    First commit

---
+++ scripts/text-style-changer.js
@@ -0,0 +1,54 @@
+const icons = Array.from(document.querySelectorAll('.icon'));
+
+let currentDivEl = document.querySelector('div[contenteditable="true"]:nth-child(1n)');
+
+let bold = false;
+let italic = false;
+let underline = false;
+
+Array.from(document.querySelectorAll('div[contenteditable="true"')).forEach((div) => {
+    div.addEventListener('focus', () => {
+        currentDivEl = div;
+    });
+});
+
+icons.forEach((icon) => {
+    icon.addEventListener('click', () => {
+        const action = icon.dataset.action;
+
+        let tag;
+
+        switch (action) {
+            case 'bold':
+                tag = 'strong';
+                break;
+
+            case 'italic':
+                tag = 'em';
+                break;
+
+            case 'underline':
+                tag = 'span style="text-decoration: underline"';
+                break;
+
+            case 'list':
+                tag = 'li';
+                break;
+
+            default:
+                tag = null;
+                break;
+        }
+
+        const selectedText = document.getSelection().toString();
+
+        if (tag == 'span style="text-decoration: underline"') {                                                   // Isso faz com que o próximo texto a ser escrito não mantenha a formatação atual
+            currentDivEl.innerHTML = currentDivEl.innerHTML.replace(selectedText, `<${tag}>${selectedText}</span><span>&nbsp;</span>`);
+        } else if (tag == 'li') {
+            currentDivEl.innerHTML = currentDivEl.innerHTML.replace(selectedText, `\u2022 ${selectedText}`);
+        } else {
+            currentDivEl.innerHTML = currentDivEl.innerHTML.replace(selectedText, `<${tag}>${selectedText}</${tag}><span>&nbsp;</span>`);
+        }
+
+    });
+})
